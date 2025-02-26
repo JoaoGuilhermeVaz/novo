8e9424a8930eb8091c8f27f86e045f8b76af0d62
JOÃO GUILHERME VAZ <joao_g_vaz@estudante.sesisenai.org.br>
2025-02-26 09:18:17 -0300

    inicio

---
+++ cpf.js
@@ -0,0 +1,37 @@
+function calculoloop(contador, cpfsodigitos){
+    let soma = 0
+    for (i=contador; i>1; i--){
+        soma += cpfsodigitos[contador-i] * i
+    }
+    let resultado = soma%11
+
+    if(
+        (resultado<2 && cpfsodigitos[contador -1]!=0)
+        || (resultado >= 2 && cpfsodigitos [contador - 1]!= 11 - resultado)
+    ){
+        return false
+    }
+    return true
+}
+
+function verificarCPF (cpf){
+    let cpfsodigitos = cpf.replaceAll('.',''). replaceAll('-','').split('')
+    if (cpfsodigitos.length != 11){
+        return 'CPF Inválido'
+    }
+    let resposta = calculoloop(10, cpfsodigitos)
+    if(!resposta){
+        return 'CPF inválido'
+    }
+    let resposta2 = calculoloop(11, cpfsodigitos)
+    if(!resposta2){
+        return 'CPF inválido'
+    }
+
+    return 'CPF Válido'
+
+}
+let cpf = '074.688.359-50'
+let cpf2 = '088.557.889-90'
+console.log (verificarCPF(cpf))
+console.log (verificarCPF(cpf2))
